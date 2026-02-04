7133139c20047a36ae4d373e1456d3b4d2205c09
MahaVishnu13031507 <mahav1303@gmail.com>
2026-02-04 20:59:11 +0530

    Create 2955-AccountBalanceAfterRoundedPurchase

---
+++ 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
@@ -0,0 +1,10 @@
+// Last updated: 2/4/2026, 8:59:10 PM
+class Solution {
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
+        if (purchaseAmount%10 < 5){
+            return 100 - (purchaseAmount/10)*10;
+        } else {
+            return 100 - ((purchaseAmount/10))*10-10 ;
+        }
+    }
+}
