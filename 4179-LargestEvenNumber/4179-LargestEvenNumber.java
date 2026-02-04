2d59b9ed8b60407e4eaadae2ddf6074717b97346
MahaVishnu13031507 <mahav1303@gmail.com>
2026-02-04 20:58:57 +0530

    Create 4179-LargestEvenNumber

---
+++ 4179-LargestEvenNumber/4179-LargestEvenNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 2/4/2026, 8:58:56 PM
+class Solution {
+    public String largestEven(String s) {
+        for (int i=s.length()-1;i>=0;i--){
+            int d=s.charAt(i)-'0';
+            if (d%2==0){
+                return s.substring(0,i+1);
+            }
+        }
+        return "";
+    }
+    
+}
