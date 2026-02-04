f4bd7cc528b7295e939a86e53cc105f0a4b3d69d
MahaVishnu13031507 <mahav1303@gmail.com>
2026-02-04 20:59:53 +0530

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.cpp
@@ -0,0 +1,16 @@
+// Last updated: 2/4/2026, 8:59:52 PM
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        int a = 0, b = 1, c = 0;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+};
